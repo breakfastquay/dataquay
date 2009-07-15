81095e2df45e69ab3a9d62e5d2e772152eba2bbf
Chris Cannam <cannam@all-day-breakfast.com>
2009-07-15 13:58:57 +0000

    * Docs, and some (not all) build fixes

--- PropertyObject.h
+++ PropertyObject.h
@@ -18,10 +18,11 @@ class Store;
  *
  * Helper class for managing properties of an object URI -- that is,
  * triples that share a common subject and possibly a common prefix
- * for the predicate.  This class provides set and get methods that
- * act directly upon the backing datastore, with (optional but
- * recommended) transaction support.  See CacheingPropertyObject for a
- * cacheing alternative.
+ * for the predicate, and that are unique (having a one-to-one
+ * relationship between (subject,predicate) and object).  This class
+ * provides set and get methods that act directly upon the backing
+ * datastore, with (optional but recommended) transaction support.
+ * See CacheingPropertyObject for a cacheing alternative.
  *
  * PropertyObject is constructed using a "property prefix" (a string)
  * and "my URI" (a URI).  The URI is used by the property object as
@@ -53,12 +54,53 @@ class Store;
 class PropertyObject
 {
 public:
+    /**
+     * Construct a PropertyObject acting on the given Store, with the
+     * given default prefix for properties and the given "subject"
+     * URI.
+     */
     PropertyObject(Store *s, QString propertyPrefix, QUrl myUri);
+
+    /**
+     * Construct a PropertyObject acting on the given Store, with the
+     * given default prefix for properties and the given "subject"
+     * URI (which will be prefix expanded).
+     */
     PropertyObject(Store *s, QString propertyPrefix, QString myUri);
 
+    /**
+     * Return true if the property object has the given property.  That
+     * is, if the store contains at least one triple whose subject and
+     * predicate match those for my URI and the expansion of the given
+     * property name.
+     */
     bool hasProperty(Transaction *tx, QString name) const;
+
+    /**
+     * Get the value of the given property.  That is, if the store
+     * contains at least one triple whose subject and predicate match
+     * those for my URI and the expansion of the given property name,
+     * convert the object part of the first such matching triple to a
+     * QVariant via Node::toVariant and return that value.  If there
+     * is no such match, return QVariant().
+     */
     QVariant getProperty(Transaction *tx, QString name) const;
+
+    /**
+     * Set the given property to the given value.  That is, first
+     * remove from the store any triples whose subject and predicate
+     * match those for my URI and the expansion of the given property
+     * name, then insert a new triple whose object part is the result
+     * of converting the given variant to a node via
+     * Node::fromVariant.
+     */
     void setProperty(Transaction *tx, QString name, QVariant value);
+
+    /**
+     * Remove the given property.  That is, remove from the store any
+     * triples whose subject and predicate match those for my URI and
+     * the expansion of the given property name.
+     */
     void removeProperty(Transaction *tx, QString name);
 
     Store *getStore(Transaction *tx) const;
