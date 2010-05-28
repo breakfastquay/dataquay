3be47e8d7c34974ffbbc70e523f5bf269735ab22
Chris Cannam <cannam@all-day-breakfast.com>
2010-05-28 21:24:28 +0100

    * doc update

--- Node.cpp
+++ Node.cpp
@@ -257,30 +257,6 @@ Node::getVariantTypeName(Uri datatype)
     else return QString();
 }
 
-/*!!! move this (and converse) to examples/ as example of encoder registration
-static
-QString
-qTimeToXsdDuration(QTime t)
-{
-    if (t.hour() != 0) {
-        return
-            QString("PT%1H%2M%3S")
-            .arg(t.hour())
-            .arg(t.minute())
-            .arg(t.second() + double(t.msec())/1000.0);
-    } else if (t.minute() != 0) {
-        return
-            QString("PT%1M%2S")
-            .arg(t.minute())
-            .arg(t.second() + double(t.msec())/1000.0);
-    } else {
-        return
-            QString("PT%1S")
-            .arg(t.second() + double(t.msec())/1000.0);
-    }
-}
-*/
-
 Node
 Node::fromVariant(const QVariant &v)
 {
